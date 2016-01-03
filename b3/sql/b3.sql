dc4ffe58a3aad51cc5335aaa4a95694e2d10027f
DutchMansProud <mfoster0206@hotmail.com>
2016-01-03 14:41:49 +0100

    -xG|b3 bot
    More ranks

--- b3/sql/b3.sql
+++ b3/sql/b3.sql
@@ -1,6 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- 
--- Database: `b3`
+-- Database: `-xG|b3 bot`
 -- 
 
 -- --------------------------------------------------------
@@ -88,11 +88,14 @@ CREATE TABLE IF NOT EXISTS groups (
 -- Dumping data for table `groups`
 -- 
 
-INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (128, 0, 'Super Admin', 'superadmin', UNIX_TIMESTAMP(), 100);
-INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (64, 0, 'Senior Admin', 'senioradmin', UNIX_TIMESTAMP(), 80);
-INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (32, 0, 'Full Admin', 'fulladmin', UNIX_TIMESTAMP(), 60);
-INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (16, 0, 'Admin', 'admin', UNIX_TIMESTAMP(), 40);
-INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (8, 0, 'Moderator', 'mod', UNIX_TIMESTAMP(), 20);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (512, 0, 'Super Admin', 'superadmin', UNIX_TIMESTAMP(), 100);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (256, 0, 'Grand Admin', 'grandadmin', UNIX_TIMESTAMP(), 90);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (128, 0, 'Senior Admin', 'senioradmin', UNIX_TIMESTAMP(), 80);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (64, 0, 'Full Admin', 'fulladmin', UNIX_TIMESTAMP(), 60);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (32, 0, 'Admin', 'admin', UNIX_TIMESTAMP(), 40);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (16, 0, 'Moderator', 'mod', UNIX_TIMESTAMP(), 20);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (8, 0, 'Member', 'member', UNIX_TIMESTAMP(), 15);
+INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (4, 0, 'Recruit', 'rec', UNIX_TIMESTAMP(), 10);
 INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (2, 0, 'Regular', 'reg', UNIX_TIMESTAMP(), 2);
 INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (1, 0, 'User', 'user', UNIX_TIMESTAMP(), 1);
 INSERT INTO `groups` (id, time_edit, name, keyword, time_add, level) VALUES (0, 0, 'Guest', 'guest', UNIX_TIMESTAMP(), 0);
